c369abd74477620e6a7c1a7c50bf10d948454e88
MartinLaptop <martinbachmor@gmail.com>
2022-09-19 00:11:58 +0200

    Scrolls only in Tetra Tab+Cleanup

--- src/main/java/net/acetheeldritchking/art_of_forging/AoFRegistry.java
+++ src/main/java/net/acetheeldritchking/art_of_forging/AoFRegistry.java
@@ -2,12 +2,9 @@ package net.acetheeldritchking.art_of_forging;
 
 
 import net.minecraft.world.item.Item;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import se.mickelus.tetra.TetraRegistries;
-import se.mickelus.tetra.items.modular.impl.ModularSingleHeadedItem;
 
 public class AoFRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ArtOfForging.MOD_ID);
--- src/main/java/net/acetheeldritchking/art_of_forging/DummyItem.java
+++ src/main/java/net/acetheeldritchking/art_of_forging/DummyItem.java
@@ -7,6 +7,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import se.mickelus.tetra.blocks.scroll.ScrollData;
 import se.mickelus.tetra.blocks.scroll.ScrollItem;
+import se.mickelus.tetra.items.TetraItemGroup;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Arrays;
@@ -23,7 +24,7 @@ public class DummyItem extends Item {
     }
 
     public void fillItemCategory(CreativeModeTab group, NonNullList<ItemStack> items) {
-        if (true) {
+        if (group == TetraItemGroup.instance) {
             items.add(this.setupSchematic("tetra/flamberge_blade", "detail:art_of_forging",new String[]{"sword/flamberge_blade"}, false, 2, 16750098, 6, 15, 4, 7));
             items.add(this.setupSchematic("single/head/halberd_head/halberd_head", "detail:art_of_forging",new String[]{"single/head/halberd_head/halberd_head"}, false, 2, 4475647, 8, 1, 4, 5));
             items.add(this.setupSchematic("sword/key_guard", "detail:art_of_forging",new String[]{"sword/key_guard"}, false, 2, 16442377, 0, 1, 9, 4));
