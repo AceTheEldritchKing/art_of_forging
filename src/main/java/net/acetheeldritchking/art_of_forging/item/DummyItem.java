d8e6d486122298f6130785d69deee265eb454525
Kristel Trujillo <solcat0414@gmail.com>
2022-10-05 12:19:50 -0400

    Fix

--- src/main/java/net/acetheeldritchking/art_of_forging/item/DummyItem.java
+++ src/main/java/net/acetheeldritchking/art_of_forging/item/DummyItem.java
@@ -17,7 +17,8 @@ import java.util.stream.Collectors;
 public class DummyItem extends Item {
     protected Minecraft minecraft;
     public DummyItem() {
-        super(new Properties().tab(se.mickelus.tetra.TetraItemGroup.instance).stacksTo(1));
+        super (new Properties ());
+        //super(new Properties().tab(se.mickelus.tetra.TetraItemGroup.instance).stacksTo(1));
     }
 
     private CreativeModeTab tetraTab;
@@ -53,17 +54,17 @@ public class DummyItem extends Item {
 
     public void fillItemCategory(CreativeModeTab group, NonNullList<ItemStack> items) {
         if (group.equals(getTetraTab())) {
-            items.add(this.setupSchematic("tetra/flamberge_blade", "detail:art_of_forging",new String[]{"sword/flamberge_blade"}, false, 2, 16750098, 6, 15, 4, 7));
-            items.add(this.setupSchematic("single/head/halberd_head/halberd_head", "detail:art_of_forging",new String[]{"single/head/halberd_head/halberd_head"}, false, 2, 4475647, 8, 1, 4, 5));
-            items.add(this.setupSchematic("sword/key_guard", "detail:art_of_forging",new String[]{"sword/key_guard"}, false, 2, 16442377, 0, 1, 9, 4));
-            items.add(this.setupSchematic("single/head/mace_head/mace_head", "detail:art_of_forging",new String[]{"single/head/mace_head/mace_head"}, false, 2, 5636192, 9, 3, 6, 2));
-            items.add(this.setupSchematic("tetra/crucible_blade", "detail:art_of_forging",new String[]{"sword/crucible_blade"}, false, 2, 16719360, 8, 7, 9, 2));
-            items.add(this.setupSchematic("bow/stave/dreadnought_stave", "detail:art_of_forging",new String[]{"bow/stave/dreadnought_stave", "bow/stave/dreadnought_cross_stave"}, false, 2, 15971103, 8, 1, 9, 5));
-            items.add(this.setupSchematic("sword/katana/katana_blade", "detail:art_of_forging",new String[]{"sword/katana/katana_blade"}, false, 2, 14417680, 5, 10, 13, 2));
-            items.add(this.setupSchematic("bow/string/compound_string", "detail:art_of_forging",new String[]{"bow/string/compound_string", "crossbow/string/compound_cross_string"}, false, 1, 1697160, 15, 13, 12, 14));
-            items.add(this.setupSchematic("utilize/hammer", "detail:art_of_forging",new String[]{"utilize/hammer"}, false, 1, 16422889, 1, 15, 12, 8));
-            items.add(this.setupSchematic("sword/katana/murasama_blade", "detail:art_of_forging",new String[]{"sword/katana/murasama_blade"}, false, 1, 12919587, 6, 7, 13, 15));
-            items.add(this.setupSchematic("tetra/rending_scissor_purple", "detail:art_of_forging",new String[]{"sword/rending_scissor_red", "sword/rending_scissor_purple"}, false, 1, 14885250, 1, 15, 2, 13));
+            items.add(this.setupSchematic("tetra/flamberge_blade", "art_of_forging",new String[]{"sword/flamberge_blade"}, false, 2, 16750098, 6, 15, 4, 7));
+            items.add(this.setupSchematic("single/head/halberd_head/halberd_head", "art_of_forging",new String[]{"single/head/halberd_head/halberd_head"}, false, 1, 4475647, 8, 1, 4, 5));
+            items.add(this.setupSchematic("sword/key_guard", "art_of_forging",new String[]{"sword/key_guard"}, false, 1, 16442377, 0, 1, 9, 4));
+            items.add(this.setupSchematic("single/head/mace_head/mace_head", "art_of_forging",new String[]{"single/head/mace_head/mace_head"}, false, 1, 5636192, 9, 3, 6, 2));
+            items.add(this.setupSchematic("tetra/crucible_blade", "art_of_forging",new String[]{"sword/crucible_blade"}, false, 2, 16719360, 8, 7, 9, 2));
+            items.add(this.setupSchematic("bow/stave/dreadnought_stave", "art_of_forging",new String[]{"bow/stave/dreadnought_stave", "bow/stave/dreadnought_cross_stave"}, false, 1, 15971103, 8, 1, 9, 5));
+            items.add(this.setupSchematic("sword/katana/katana_blade", "art_of_forging",new String[]{"sword/katana/katana_blade"}, false, 2, 14417680, 5, 10, 13, 2));
+            items.add(this.setupSchematic("bow/string/compound_string", "art_of_forging",new String[]{"bow/string/compound_string", "crossbow/string/compound_cross_string"}, false, 1, 1697160, 15, 13, 12, 14));
+            items.add(this.setupSchematic("utilize/hammer", "art_of_forging",new String[]{"utilize/hammer"}, false, 2, 16422889, 1, 15, 12, 8));
+            items.add(this.setupSchematic("sword/katana/murasama_blade", "otherworldly",new String[]{"sword/katana/murasama_blade"}, false, 2, 12919587, 6, 7, 13, 15));
+            items.add(this.setupSchematic("tetra/rending_scissor_purple", "otherworldly",new String[]{"sword/rending_scissor_red", "sword/rending_scissor_purple"}, false, 2, 14885250, 1, 15, 2, 13));
         }
     }
 
