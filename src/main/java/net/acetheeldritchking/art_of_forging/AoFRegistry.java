fae79977f0720f783c526cfe0e6ee0bb7cf690f3
Kristel Trujillo <solcat0414@gmail.com>
2022-10-19 18:15:03 -0400

    WIP: Making relic item, it's a little broken and breaks the mod but that's ok we still love it <3

--- src/main/java/net/acetheeldritchking/art_of_forging/AoFRegistry.java
+++ src/main/java/net/acetheeldritchking/art_of_forging/AoFRegistry.java
@@ -2,6 +2,7 @@ package net.acetheeldritchking.art_of_forging;
 
 
 import net.acetheeldritchking.art_of_forging.item.DummyItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,4 +14,8 @@ public class AoFRegistry {
     public static final RegistryObject<Item> DUMMY_ITEM = ITEMS.register("dummyitem", DummyItem::new);
 
     public static final RegistryObject<Item> ModularRelic = ITEMS.register("modular_relic", ModularRelic::new);
+
+    // Relic Thingy because I'm bored
+    public static final RegistryObject<Item> BASIC_RELIC = ITEMS.register("basic_relic",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 }
--- src/main/java/net/acetheeldritchking/art_of_forging/ArtOfForging.java
+++ src/main/java/net/acetheeldritchking/art_of_forging/ArtOfForging.java
@@ -20,6 +20,7 @@ public class ArtOfForging
         var bus = FMLJavaModLoadingContext.get().getModEventBus();
         AoFRegistry.ITEMS.register(bus);
         // Register the setup method for modloading
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         //Test comment for first commit+push
         // Register ourselves for server and other game events we are interested in
