1fcadb1d7931f56837d597380ea1a172e404ca7e
Kristel Trujillo <solcat0414@gmail.com>
2023-01-29 15:26:04 -0500

    Updated to 1.19.2 and removed all Nether Star Sockets (RIP)

--- src/main/java/net/acetheeldritchking/art_of_forging/ArtOfForging.java
+++ src/main/java/net/acetheeldritchking/art_of_forging/ArtOfForging.java
@@ -1,35 +1,53 @@
 package net.acetheeldritchking.art_of_forging;
 
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ArtOfForging.MOD_ID)
 public class ArtOfForging
 {
     public static final String MOD_ID = "art_of_forging";
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ArtOfForging() {
+    public ArtOfForging()
+    {
+        // IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         var bus = FMLJavaModLoadingContext.get().getModEventBus();
         AoFRegistry.ITEMS.register(bus);
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        //Test comment for first commit+push
+        // Register the commonSetup method for modloading
+        // modEventBus.addListener(this::commonSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
+    private void commonSetup(final FMLCommonSetupEvent event)
     {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
     }
 }
