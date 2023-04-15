eda4f73c1bd73d8e4e1d0ce2505b1309f6a8afe5
Kristel Trujillo <solcat0414@gmail.com>
2023-04-14 23:29:02 -0400

    Added two new items: Forged Steel and Resonant Alloy. Refactored lang files. Refactored socket modules to better fit in with 1.19.2 Tetra. Added new module: Echo Fuller. Fixed advancements relating to the Rending Scissors. Nerfed several modules by removing True Sweep from them. Added crafting recipes for Forged wall, pillar, platform, and platform slab. Added Forged Steel as a material. Removed assets relating to the Nether Star socket.

--- src/main/java/net/acetheeldritchking/art_of_forging/AoFRegistry.java
+++ src/main/java/net/acetheeldritchking/art_of_forging/AoFRegistry.java
@@ -1,7 +1,7 @@
 package net.acetheeldritchking.art_of_forging;
 
-
 import net.acetheeldritchking.art_of_forging.item.DummyItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,5 +9,12 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class AoFRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ArtOfForging.MOD_ID);
+
     public static final RegistryObject<Item> DUMMY_ITEM = ITEMS.register("dummyitem", DummyItem::new);
+
+    public static final RegistryObject<Item> FORGED_STEEL_INGOT = ITEMS.register("forged_steel_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
+
+    public static final RegistryObject<Item> RESONANT_ALLOY = ITEMS.register("resonant_alloy",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
 }
